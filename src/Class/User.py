f39de272363e16506d6a306d602eee37f072581e
Harshitha G S <harshitha.g.s@intimetec.com>
2024-06-01 16:07:28 +0530

    adding admin CRUD Module

--- src/Class/User.py
+++ src/Class/User.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from SQLConnect import create_connection, execute_read_query
 
 class User:
--- src/SQLConnect.py
+++ src/SQLConnect.py
@@ -11,27 +11,22 @@ def execute_read_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-def create_connection(host_name, user_name, user_password, db_name):
+def create_connection():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-        #select_users = "SELECT RoleName FROM cafeteria.role WHERE RoleID = 1;"
-        '''select_users = "SELECT * FROM cafeteria.role "
-        users = execute_read_query(connection, select_users)
-
-        for user in users:
-            print(f"'{user[1]}' is the role " )'''
+        host="localhost",
+        user="root",
+        password= "Harshitha@555",
+        database="cafeteria"
+    )
         
     except Error as e:
         print(f"The error '{e}' occurred")
 
     return connection
+
+
 if __name__ == "__main__":
 # Replace the parameters with your MySQL server details
     connection = create_connection("localhost", "root", "Harshitha@555", "cafeteria")
